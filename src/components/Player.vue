47c59189db849f0ef3f334b2710363e5e257fb8d
henrique11varela <henrique11varela@gmail.com>
2023-08-22 22:55:47 +0100

    rotation fix

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -3,44 +3,14 @@ export default {
     props: ['playerNumber', 'quantPlayers'],
     data() {
         return {
-            PlayerNumber: Number(this.playerNumber),
-            QuantPlayers: Number(this.quantPlayers),
-            PlayerId: `Player${this.playerNumber}`
-        }
-    },
-    mounted(){
-        if (this.PlayerNumber == this.QuantPlayers && this.QuantPlayers % 2) {
-            let outterElement = document.querySelector(`#${this.PlayerId}`)
-            let innerElement = outterElement.querySelector(`.innerDiv`)
-            innerElement.style.width = outterElement.style.height + "px"
-            innerElement.style.height = outterElement.style.width + "px"
-        }
-    },
-    computed: {
-        cssVars() {
-            let rotate = "0deg";
-            if (this.PlayerNumber % 2) {
-                rotate = "180deg"
-            }
-            else {
-                rotate = "0deg"
-            }
-            if (this.PlayerNumber == this.QuantPlayers && this.QuantPlayers % 2) {
-                rotate = "-90deg"
-            }
-            return {
-                '--rotation': rotate
-            }
         }
     }
 }
 </script>
 
 <template>
-    <div class="Player" v-bind:id="PlayerId" v-bind:style="cssVars">
-        <div class="innerDiv">
-            <p>Lorem</p>
-        </div>
+    <div class="Player">
+        <p>Lorem</p>
     </div>
 </template>
 
@@ -49,40 +19,14 @@ export default {
     width: 100%;
     height: 100%;
     /* test */
-    width: calc(100% - 1px);
-    height: calc(100% - 1px);
-    background-color: lightblue;
-    border: 1px solid red;
-}
-
-.innerDiv {
     display: flex;
     justify-content: center;
     align-items: center;
-    rotate: var(--rotation);
+    background-color: lightblue;
 }
 
-#Player1 {
-    grid-area: player1;
-}
-
-#Player2 {
-    grid-area: player2;
-}
-
-#Player3 {
-    grid-area: player3;
-}
-
-#Player4 {
-    grid-area: player4;
-}
-
-#Player5 {
-    grid-area: player5;
-}
-
-#Player6 {
-    grid-area: player6;
+.innerDiv {
+    display: grid;
+    place-items: center;
 }
 </style>
--- src/components/PlayerGrid.vue
+++ src/components/PlayerGrid.vue
@@ -4,37 +4,76 @@ export default {
     props: ['quantPlayers'],
     data() {
         return {
-            GridClass: `PlayerCount${this.quantPlayers}`
+
         }
     },
-    mounted(){
-        let gridEl = document.querySelector('.PlayerGrid')
-        let gridParentEl = gridEl.parentElement
-        let width = gridParentEl.offsetWidth
-        let height = gridParentEl.offsetHeight
-        console.log(width, height)
-        if (height > width) {
-            gridEl.style.width = height + "px"
-            gridEl.style.height = width + "px"
-            // gridEl.style.transform = "rotate(90deg)"
+    mounted() {
+        // playergrid rotate
+        let playerGrid = this.$refs.pg
+        let playerGridParent = playerGrid.parentElement
+        if (playerGridParent.offsetHeight > playerGridParent.offsetWidth) {
+            rotateResizeElement(playerGrid, 90)
         }
+        //player rotate
+        for (let i = 0; i < this.quantPlayers; i++) {
+            let deg = 0
+            if (!(i % 2)) {
+                deg = 180
+                if (i == this.quantPlayers - 1) {
+                    deg = -90
+                    this.$refs.players[i].style.top = (this.$refs.players[i].offsetHeight - this.$refs.players[i].offsetWidth) / 2 + "px"
+                    this.$refs.players[i].style.left = (this.$refs.players[i].offsetWidth - this.$refs.players[i].offsetHeight) / 2 + "px"
+                }
+            }
+            rotateResizeElement(this.$refs.players[i], deg)
+        }
+
     },
     components: { Player },
+}
+function rotateResizeElement(element, degrees = 0) {
+    if (degrees) {
+        element.style.transform += ` rotate(${degrees}deg)`
+    }
+    if (degrees == 90 || degrees == -90) {
+        let parent = element.parentElement
+        let heightSpace = parent.offsetHeight
+        let widthSpace = parent.offsetWidth
+        element.style.height = `${widthSpace}px`
+        element.style.width = `${heightSpace}px`
+    }
 }
 </script>
 
 <template>
-        <div class="PlayerGrid" v-bind:class="GridClass">
-            <Player v-for="index in Number(quantPlayers)" v-bind:player-number="index"
-            v-bind:quant-players="quantPlayers" />
+    <div class="outer">
+        <div ref="pg" class="PlayerGrid" v-bind:class="`PlayerCount${this.quantPlayers}`">
+            <div v-for="player in Number(quantPlayers)" v-bind:id="`Player${player}`" class="Player">
+                <div :ref="`players`">
+                    <Player />
+                </div>
+            </div>
         </div>
+    </div>
 </template>
 
 <style scoped>
-.PlayerGrid {
-    width: 100%;
+.outer {
     height: 100%;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+}
+
+.PlayerGrid {
+    height: 100%;
+    width: 100%;
     display: grid;
+    place-items: center;
+    background-color: red;
+    position: absolute;
 }
 
 .PlayerCount1 {
@@ -70,9 +109,42 @@ export default {
         "player1 player3 player5"
         "player2 player4 player6";
 }
-@media screen and (max-aspect-ratio: 1) {
-    .PlayerGrid {
-        rotate: 90deg;
-    }
+
+.Player,
+.Player>div {
+    height: 100%;
+    width: 100%;
+}
+
+.Player {
+    position: relative;
+}
+
+.Player>div {
+    position: absolute;
+}
+
+#Player1 {
+    grid-area: player1;
+}
+
+#Player2 {
+    grid-area: player2;
+}
+
+#Player3 {
+    grid-area: player3;
+}
+
+#Player4 {
+    grid-area: player4;
+}
+
+#Player5 {
+    grid-area: player5;
+}
+
+#Player6 {
+    grid-area: player6;
 }
 </style>
